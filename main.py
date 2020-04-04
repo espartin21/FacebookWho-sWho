11feca86ad79d0f8372e171710c9a4aec80157c5
Ethan Partin <espartin98@gmail.com>
2020-04-03 19:12:38 -0500

    Add util functions

--- main.py
+++ main.py
@@ -1,50 +1,17 @@
 import pandas as pd
-
-
-def mostLikedUser(facebookDF):
-    df = facebookDF[facebookDF.likes_received ==
-                    facebookDF['likes_received'].max()]
-
-    return {'userid': (int)(df.iloc[0]['userid']), 'likes_received': (int)(df.iloc[0]['likes_received'])}
-
-
-def mostLikesHandedOut(facebookDF):
-    df = facebookDF[facebookDF.likes == facebookDF['likes'].max()]
-
-    return {'userid': (int)(df.iloc[0]['userid']), 'likes': (int)(df.iloc[0]['likes'])}
-
-
-def friendliestGuy(facebookDF):
-    df = facebookDF[facebookDF.friendships_initiated ==
-                    facebookDF['friendships_initiated'].max()]
-
-    return {'userid': (int)(df.iloc[0]['userid']), 'friendships_initiated': (int)(df.iloc[0]['friendships_initiated'])}
-
-
-def guyEveryoneWantsToKnow(facebookDF):
-    df = facebookDF[facebookDF.friend_count ==
-                    facebookDF['friend_count'].max()]
-
-    return {'userid': (int)(df.iloc[0]['userid']), 'friend_count': (int)(df.iloc[0]['friend_count'])}
-
-
-def longestTimeOnFacebook(facebookDF):
-    df = facebookDF[facebookDF.tenure == facebookDF['tenure'].max()]
-
-    return {'userid': (int)(df.iloc[0]['userid']), 'tenure': (int)(df.iloc[0]['tenure'])}
+from utils import *
 
 
 if __name__ == "__main__":
     facebookDF = pd.read_csv('data/pseudo_facebook.csv')
 
-    mostLiked = mostLikedUser(facebookDF)
-    mostLikesGiven = mostLikesHandedOut(facebookDF)
-    friendly = friendliestGuy(facebookDF)
-    coolestCat = guyEveryoneWantsToKnow(facebookDF)
-    alwaysOnline = longestTimeOnFacebook(facebookDF)
-
-    print(mostLiked)
-    print(mostLikesGiven)
-    print(friendly)
-    print(coolestCat)
-    print(alwaysOnline)
+    print(mostLikedUser(facebookDF))
+    print(mostLikesHandedOut(facebookDF))
+    print(friendliestGuy(facebookDF))
+    print(guyEveryoneWantsToKnow(facebookDF))
+    print(longestTimeOnFacebook(facebookDF))
+    print(leastLikedUser(facebookDF))
+    print(leastLikesHandedOut(facebookDF))
+    print(unfriendliestGuy(facebookDF))
+    print(guyNobodyWantsToKnow(facebookDF))
+    print(shortestTimeOnFacebook(facebookDF))
---
+++ utils.py
@@ -0,0 +1,67 @@
+import pandas
+
+
+def mostLikedUser(facebookDF):
+    df = facebookDF[facebookDF.likes_received ==
+                    facebookDF['likes_received'].max()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'likes_received': (int)(df.iloc[0]['likes_received'])}
+
+
+def mostLikesHandedOut(facebookDF):
+    df = facebookDF[facebookDF.likes == facebookDF['likes'].max()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'likes': (int)(df.iloc[0]['likes'])}
+
+
+def friendliestGuy(facebookDF):
+    df = facebookDF[facebookDF.friendships_initiated ==
+                    facebookDF['friendships_initiated'].max()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'friendships_initiated': (int)(df.iloc[0]['friendships_initiated'])}
+
+
+def guyEveryoneWantsToKnow(facebookDF):
+    df = facebookDF[facebookDF.friend_count ==
+                    facebookDF['friend_count'].max()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'friend_count': (int)(df.iloc[0]['friend_count'])}
+
+
+def longestTimeOnFacebook(facebookDF):
+    df = facebookDF[facebookDF.tenure == facebookDF['tenure'].max()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'tenure': (int)(df.iloc[0]['tenure'])}
+
+
+def leastLikedUser(facebookDF):
+    df = facebookDF[facebookDF.likes_received ==
+                    facebookDF['likes_received'].min()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'likes_received': (int)(df.iloc[0]['likes_received'])}
+
+
+def leastLikesHandedOut(facebookDF):
+    df = facebookDF[facebookDF.likes == facebookDF['likes'].min()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'likes': (int)(df.iloc[0]['likes'])}
+
+
+def unfriendliestGuy(facebookDF):
+    df = facebookDF[facebookDF.friendships_initiated ==
+                    facebookDF['friendships_initiated'].min()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'friendships_initiated': (int)(df.iloc[0]['friendships_initiated'])}
+
+
+def guyNobodyWantsToKnow(facebookDF):
+    df = facebookDF[facebookDF.friend_count ==
+                    facebookDF['friend_count'].min()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'friend_count': (int)(df.iloc[0]['friend_count'])}
+
+
+def shortestTimeOnFacebook(facebookDF):
+    df = facebookDF[facebookDF.tenure == facebookDF['tenure'].min()]
+
+    return {'userid': (int)(df.iloc[0]['userid']), 'tenure': (int)(df.iloc[0]['tenure'])}
